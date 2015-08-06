900941e7adeaa674ffbc2cefff1d316fafe6d39f
Nick Ts <fobdy@yawny.ru>
2015-08-06 20:05:21 +0300

    Ok

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 let two = require('two.js/build/two.clean');
 let renderer = require('./lib/renderer');
+let graphlib = require('graphlib');
 
 exports.test = window.TEST = () => {
   let two = new Two({
