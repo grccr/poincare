a7abd25c4cd95e7a714e8d0efedd8451380c53e1
Даниил Потапов <sharth23@gmail.com>
2016-09-13 16:33:31 +0300

    arrow pos dependson nodes.radius

--- src/poincare/plugins/directions/index.js
+++ src/poincare/plugins/directions/index.js
@@ -53,7 +53,7 @@ export default class Directions extends Plugin {
   _render() {
     const core = this._pn.core;
     const scale = this._pn.zoom.truncatedScale();
-    const offset = this.OFFSET_FACTOR * scale;
+    const offset = this._pn._options.nodes.radius;//this.OFFSET_FACTOR * scale;
     core.eachLink((id) => {
       const link = core.link(id);
 
